a7638a959c2f350d9c37d60ffc093b171f78a9e6
Talha86-ux <rjtalha555@gmail.com>
2026-07-17 03:27:01 +0500

    Implement turbo streams

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,14 +23,13 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: "Task was successfully created." }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new, status: :unprocessable_content }
-        format.json { render json: @task.errors, status: :unprocessable_content }
+    if @task.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to tasks_path, notice: "Task was successfully created." }
       end
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
